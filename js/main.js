3a323e319714e4b46afbb2a6921c6276cc59244f
CarolinaTapia <cstapiarubio@gmail.com>
2017-05-15 11:59:24 -0400

    hice la funcion agregaposteo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
 function agregaPosteo() {
-//escribe aca el codigo
+var containerPosteos=document.getElementById("contenedor-posteos");
+var nombreFF=document.getElementById("nombre").value;
+var contenidoFF=document.getElementById("cajaposteos").value;
+var nuevoPost=document.createElement("div");
+var contenedorNombre=document.createElement("strong");
+var contenedorPost=document.createElement("p");
+var separacion=document.createElement("hr");
+var parrafCorazon =document.createElement("p");
+var i=document.createElement("i");
+parrafCorazon.appenChild(i);
+parrafCorazon.setAttribute("class", "corazon");
+i.setAttribute("class", "fa fa-heart");
+i.setAttribute("arial-hidden", "true");
+var nodoNombre=document.createTextNode(nombreFF + " escribió:");
+var nodoPosteo=document.createTextNode(contenidoFF);
+contenedorNombre.appenChild(nodoNombre);
+contenedorPost.appenChild(nodoPosteo);
+nuevoPost.appenChild(contenedorNombre);
+nuevoPost.appenChild(contenedorPost);
+nuevoPost.appenChild(parrafCorazon);
+nuevoPost.appenChild(separacion);
+i.addEventListener("click", function(){
+	i.classList.toogle("rojo");
+});
+nuevoPost.setAttribute("class", "posteo");
+containerPosteos.appenChild(nuevoPost);
+document.getElementById("nombre").value="";
+document.getElementById("cajaposteos").value="";
 }
+agregaPosteo();
